72f9cdde5adf767d5dd426e316281ad23a3445c6
Agustin <agualvila@gmail.com>
2021-09-03 13:57:29 -0300

    changed controller, from Class static async TO async arrow

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,25 +1,28 @@
 const User = require('../models/userModel');
 
-module.exports = class UserCtrl {
-    static async getUsers(req, res) {
-        try {
-            const users = await User.findAll();
-            res.send(users);
-            //console.log(users);
-        } catch (error) {
-            res.status(500).send(error.message);
-            console.log(error.message);
-        }
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.send(users);
+        //console.log(users);
+    } catch (error) {
+        res.status(500).send(error.message);
+        console.log(error.message);
     }
+}
 
-    static async createUser(req, res) {
-        try {
-            const response = await User.create({ firstname: "Jane", lastname: "Doe", age: 45, email: 'asd@qwerty.com' });
-            res.send(response);
-            //console.log(response);
-        } catch (error) {
-            res.status(500).send(error.message);
-            console.log(error.message);
-        }
+const createUser = async (req, res) => {
+    try {
+        const response = await User.create({ firstname: "Jane", lastname: "Doe", age: 45, email: 'asd@qwerty.com' });
+        res.send(response);
+        //console.log(response);
+    } catch (error) {
+        res.status(500).send(error.message);
+        console.log(error.message);
     }
+}
+
+module.exports = {
+    getUsers,
+    createUser
 }
